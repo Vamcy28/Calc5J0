8e9ea4c5322281242259185fdc1e3365d2978976
Vamcy28 <123228601+Vamcy28@users.noreply.github.com>
2023-03-09 15:24:35 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,16 @@
+function add(x,y) {
+    return x + y;
+};
+function sub(x,y) {
+    return x - y;
+};
+function mult(x,y) {
+    return x * y;
+};
+function div(x,y){
+    return x / y;
+};
+exports.add=add;
+exports.subtract=sub;
+exports.div=div;
+exports.mult=mult;
